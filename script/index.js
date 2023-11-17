82fee24b25937d362a904da25afbc1f7c69ebcf9
JoaoVictorT <joaovicteixeira123@gmail.com>
2023-11-16 22:18:25 -0300

    Portfolio

---
+++ script/index.js
@@ -0,0 +1,25 @@
+document.getElementById("id_icn_github").addEventListener("click", github);
+document.getElementById("id_icn_linkedin").addEventListener("click", linkedin);
+document.getElementById("id_icn_telefone").addEventListener("click", telefone);
+document.getElementById("id_icn_email").addEventListener("click", email);
+
+
+
+
+
+
+
+
+
+function github(){
+    window.location.href = "https://github.com/JoaoVictorT";
+}
+function linkedin(){
+    window.location.href ="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-68444a211/";
+}
+function telefone(){
+    window.location.href ="https://wa.me/5513996380107";
+}
+function email(){
+    window.location.href ="mailto:joaovicteixeira123@gmail.com";
+}
